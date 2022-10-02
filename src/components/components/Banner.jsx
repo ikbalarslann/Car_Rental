1b2e5006fb62fdeefd3403c543672c38328c19e9
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2022-10-03 02:23:24 +0300

    Banner component

--- src/components/components/Banner.jsx
+++ src/components/components/Banner.jsx
@@ -1,5 +1,16 @@
 const Banner = () => {
-  return <h2>hey i am Banner component</h2>;
+  return (
+    <div className="banner">
+      <h2 className="banner__title">hey i am Banner component</h2>
+      <h1 className="banner__main-title">
+        Save big with our cheap car rental!
+      </h1>
+      <h4 className="banner__subtitle">
+        Top Airports. Local Suppliers.{" "}
+        <strong className="banner__strong-text">24/7</strong> Support.
+      </h4>
+    </div>
+  );
 };
 
 export default Banner;
