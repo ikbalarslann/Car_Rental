25a5244ce60212564fd1798ef67b9b7c0cc2fde8
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2022-10-03 03:20:41 +0300

    Testomonials & ChooseUs component

--- src/components/components/ChooseUs.jsx
+++ src/components/components/ChooseUs.jsx
@@ -1,4 +1,55 @@
 const ChooseUs = () => {
-  return <h2>hey i am Choose us component</h2>;
+  return (
+    <div className="chooseUs">
+      <h2 className="chooseUs__title">hey i am Choose us component</h2>
+      <img src="" alt="choose us image" className="chooseUs__image" />
+      <div className="chooseUs__content">
+        <h4 className="chooseUs__subtitle">Why Choose Us</h4>
+        <h1 className="chooseUs__main-title">
+          Best valued deals you will ever find
+        </h1>
+        <p className="chooseUs__description">
+          Discover the best deals you'll ever find with our unbeatable offers.
+          we're dedicated to providing you with the best value for your money,
+          so you can enjoy top-quality services and products without breaking
+          the bank. Our deals are designed to give you the ultimate renting
+          experience, so don't miss out on your chance to save big.
+        </p>
+        <button className="chooseUs__button">Find Details</button>
+      </div>
+      <div className="chooseUs__features">
+        <div className="chooseUs__feature">
+          <img src="" alt="car icon" className="chooseUs__icon" />
+          <div className="chooseUs__feature-content">
+            <h3 className="chooseUs__feature-title">Cross Country Drive</h3>
+            <p className="chooseUs__feature-description">
+              Take your driving experience to the next level with our top-notch
+              vehicles for your cross-country adventures.
+            </p>
+          </div>
+        </div>
+        <div className="chooseUs__feature">
+          <img src="" alt="coin icon" className="chooseUs__icon" />
+          <div className="chooseUs__feature-content">
+            <h3 className="chooseUs__feature-title">All Inclusive Pricing</h3>
+            <p className="chooseUs__feature-description">
+              Get everything you need in one convenient, transparent price with
+              our all-inclusive pricing policy.
+            </p>
+          </div>
+        </div>
+        <div className="chooseUs__feature">
+          <img src="" alt="holding coin icon" className="chooseUs__icon" />
+          <div className="chooseUs__feature-content">
+            <h3 className="chooseUs__feature-title">No Hidden Charges</h3>
+            <p className="chooseUs__feature-description">
+              Enjoy peace of mind with our no hidden charges policy. We believe
+              in transparent and honest pricing.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ChooseUs;
--- src/components/components/Testimonial.jsx
+++ src/components/components/Testimonial.jsx
@@ -1,4 +1,35 @@
+import React from "react";
+import { testomonialData } from "./testomonialData";
+
 const Testimonial = () => {
-  return <h2>hey i am Testimonial component</h2>;
+  return (
+    <div className="testomonial">
+      <h2 className="testomonial__title">hey i am Testimonial component</h2>
+      <h4 className="testomonial__subtitle">Reviewed by People</h4>
+      <h1 className="testomonial__main-title">Client's Testimonials</h1>
+      <p className="testomonial__description">
+        Discover the positive impact we've made on our clients by reading
+        through their <br />
+        testimonials. Our clients have experienced our service and results, and
+        they're eager to share <br />
+        their positive experiences with you.
+      </p>
+      <div>
+        {testomonialData.map((element, index) => (
+          <div key={index} className="testomonial-item">
+            <p className="testomonial-item__review">{element.review}</p>
+            <img
+              src={element.img}
+              alt="Client"
+              className="testomonial-item__image"
+            />
+            <h4 className="testomonial-item__name">{element.name}</h4>
+            <p className="testomonial-item__location">{element.location}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
+
 export default Testimonial;
