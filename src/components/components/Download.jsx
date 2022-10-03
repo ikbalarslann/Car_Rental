a34b884bc1000afde3a55866b999f9c7cadef73b
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2022-10-03 04:21:26 +0300

    Downland component

--- src/components/components/Download.jsx
+++ src/components/components/Download.jsx
@@ -1,4 +1,23 @@
 const Downland = () => {
-  return <h2>hey i am Downland component</h2>;
+  return (
+    <div className="downland">
+      <div className="downland__content">
+        <h2 className="downland__title">hey i am Downland component</h2>
+        <h1 className="downland__subtitle">
+          Download our app to get <br /> most out of it
+        </h1>
+        <p className="downland__description">
+          Thrown shy denote ten ladies though ask saw. Or by to he going think
+          order event music. Incommode so intention defective at convinced. Led
+          income months itself and houses you.
+        </p>
+        <div className="downland__stores">
+          <img src="" alt="play Store" className="downland__store-image" />
+          <img src="" alt="app Store" className="downland__store-image" />
+        </div>
+      </div>
+      <img src="" alt="phone photo" className="downland__phone-image" />
+    </div>
+  );
 };
 export default Downland;
--- src/components/components/Testimonial.jsx
+++ src/components/components/Testimonial.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-import { testomonialData } from "./testomonialData";
+import HarryPoter from "../../assets/img/testomonials/pfp1.jpg";
+import RonRizzly from "../../assets/img/testomonials/pfp2.jpg";
 
 const Testimonial = () => {
+  const testomonialData = [
+    {
+      name: "Harry Poter",
+      location: "Istanbul",
+      img: HarryPoter,
+      review:
+        "We rented a car from this website and had an amazing experience! The booking was easy and the rental rates were very affordable. ",
+    },
+    {
+      name: "Ron Rizzy",
+      location: "Ankara",
+      img: RonRizzly,
+      review:
+        "The car was in great condition and made our trip even better. Highly recommend for this car rental website!",
+    },
+  ];
+
   return (
     <div className="testomonial">
       <h2 className="testomonial__title">hey i am Testimonial component</h2>
